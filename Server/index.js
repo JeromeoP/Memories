6ca5a3cb1a767a242c35b2867e39a67217bd98d5
Jerome Planken <jeromeplankenn@hotmail.com>
2022-05-21 18:59:36 +0200

    [WIP] Fixed some variables

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import monGoose from 'mongoose';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit : "30mb", extended: true}));
@@ -15,11 +16,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jeromeop:jeromeo1234@cluster0.te2tn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://jeromeop:jeromeo1234@cluster0.te2tn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
-
-

60daebdf3346c5d83f2a258e7373bfa96de886a0
Jerome Planken <jeromeplankenn@hotmail.com>
2022-05-21 17:33:29 +0200

    [WIP] Able to remove posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,11 +5,13 @@ import ThumpUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import {useDispatch} from 'react-redux';
 
+import {deletePost} from '../../../actions/posts';
 
 const Post = ({post, setCurrentId}) => {
    const classes = useStyles();
-
+   const dispatch = useDispatch();
 
 
 
@@ -45,7 +47,7 @@ const Post = ({post, setCurrentId}) => {
           {post.likeCount}
         </Button>
 
-        <Button size = "small" color = "primary" onClick={() => {} }>
+        <Button size = "small" color = "primary" onClick={() => dispatch(deletePost(post._id)) }>
           <DeleteIcon fontSize = "small"/>
           Delete
         </Button>

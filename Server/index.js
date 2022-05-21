e4a2d6d409f162d1c18a69ef550dc1ab8d5f4c2b
Jerome Planken <jeromeplankenn@hotmail.com>
2022-05-21 19:05:49 +0200

    [WIP] Finally fixed the issue with .env

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://jeromeop:jeromeo1234@cluster0.te2tn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
